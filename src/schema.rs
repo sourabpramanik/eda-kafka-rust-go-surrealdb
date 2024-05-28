299aa32a6fb50c2427b8c6cf4993850709e8410d
sourabpramanik <shubpramanik241@gmail.com>
2024-05-28 07:00:32 +0530

    fix: add serialize macro

--- src/schema.rs
+++ src/schema.rs
@@ -1,7 +1,7 @@
 use serde::{Deserialize, Serialize};
 use surrealdb::sql::Id;
 
-#[derive(Debug, Deserialize)]
+#[derive(Debug, Deserialize, Serialize)]
 pub struct Product {
     pub id: ProductThing,
     pub name: String,
@@ -9,7 +9,7 @@ pub struct Product {
     pub units: u16,
 }
 
-#[derive(Debug, Deserialize)]
+#[derive(Debug, Deserialize, Serialize)]
 struct ProductThing {
     id: Id,
 }
