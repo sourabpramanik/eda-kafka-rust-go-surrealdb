db77d17250696464cfa61ff062ae32396f998cd0
sourabpramanik <shubpramanik241@gmail.com>
2024-06-01 16:26:07 +0530

    feat: add env and comments

--- inventory/src/main.rs
+++ inventory/src/main.rs
@@ -1,10 +1,11 @@
 mod kafka;
 mod schema;
 
-use std::time::Duration;
+use std::{env, time::Duration};
 
 use crate::schema::Product;
 use actix_web::{web, App, HttpResponse, HttpServer, Responder};
+use dotenv::dotenv;
 use futures_util::StreamExt;
 use kafka::producer;
 use rdkafka::{
@@ -26,31 +27,44 @@ struct State {
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    let db = Surreal::new::<Ws>("127.0.0.1:8000")
+    dotenv().ok();
+
+    // LOAD ENV VARS
+    let surreal_url = env::var("SURREAL_URL").expect("SURREAL_URL must be set.");
+    let surreal_ns = env::var("SURREAL_NS").expect("SURREAL_NS must be set.");
+    let surreal_db = env::var("SURREAL_DB").expect("SURREAL_DB must be set.");
+    let surreal_user = env::var("SURREAL_USER").expect("SURREAL_USER must be set.");
+    let surreal_password = env::var("SURREAL_PW").expect("SURREAL_PW must be set.");
+    let kafka_broker = env::var("KAFKA_BROKER").expect("KAFKA_BROKER must be set.");
+
+    // INIT DATABASE
+    let db = Surreal::new::<Ws>(&surreal_url)
         .await
         .expect("Failed to connect to the Surreal client");
-
     db.signin(Root {
-        username: "root",
-        password: "root",
+        username: &surreal_user,
+        password: &surreal_password,
     })
     .await
     .expect("Failed to authenticate");
-
-    db.use_ns("foo")
-        .use_db("ecommerce")
+    db.use_ns(surreal_ns)
+        .use_db(surreal_db)
         .await
         .expect("Failed to access the Database");
 
     let db_clone = db.clone();
-    let stock_producer = producer("localhost:9092").await;
 
+    // CREATE KAFKA PRODUCER
+    let stock_producer = producer(&kafka_broker).await;
+
+    // SPAWN A NEW THREAD TO EXECUTE KAFKA PRODUCER
     task::spawn(async move {
         stream_stock_changes(&db_clone, &stock_producer)
             .await
             .expect("failed to stream");
     });
 
+    // EXECUTE SERVER
     HttpServer::new(move || {
         App::new()
             .app_data(web::Data::new(State { db: db.to_owned() }))
@@ -68,6 +82,7 @@ async fn main() -> std::io::Result<()> {
     .await
 }
 
+// FETCH ALL PRODUCTS FROM INVENTORY
 async fn get_inventory_products(state: web::Data<State>) -> impl Responder {
     let db = &state.db;
 
@@ -81,6 +96,8 @@ async fn get_inventory_products(state: web::Data<State>) -> impl Responder {
     HttpResponse::Ok().json(products)
 }
 
+// LIVE STREAM STOCK UPDATES
+// PRODUCE MESSAGES FOR EVERY STOCK UPDATE EVENT
 async fn stream_stock_changes(
     db: &Surreal<Client>,
     stock_producer: &FutureProducer,
@@ -114,6 +131,7 @@ async fn stream_stock_changes(
     Ok(())
 }
 
+// VALIDATE AND UPDATE STOCKS
 async fn update_stock(
     product_id: web::Path<String>,
     state: web::Data<State>,
